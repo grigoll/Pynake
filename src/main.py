c23521a379b9508974714648ea0090eee481c0cf
Grigol <38917761+grigoll@users.noreply.github.com>
2021-03-27 20:17:03 +0400

    updates

--- src/main.py
+++ src/main.py
@@ -2,10 +2,10 @@ import arcade
 from random import randrange
 from constants import *
 
-# width & height should be multiple of SQUARE_SIZE
+# width & height should be multiple of TILE_SIZE
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-SQUARE_SIZE = 50
+TILE_SIZE = 50
 
 UP = arcade.key.UP
 RIGHT = arcade.key.RIGHT
@@ -22,64 +22,96 @@ OPPOSITE = {
 
 
 class Apple():
-    def __init__(self, size):
-        self.size = size
-        self.respawn()
+    def __init__(self, window_dimensions, tile_size, snake_placement):
+        self.tile_size = tile_size
+        self.window_dimensions = window_dimensions
 
-    def draw(self):
-        arcade.draw_circle_filled(
-            self.x, self.y, self.size / 2, arcade.color.RED
+        self.size = tile_size
+
+        sprite_orig_size = 100
+        # this is how much sprite is misaligned on Y axis when shown by original size
+        sprite_offset_y = 36
+        # magic number to push sprite upwards, otherwise it's a bit off by default
+        self.offset_y = sprite_offset_y * tile_size / sprite_orig_size
+
+        x,  y = self.new_position(snake_placement)
+
+        self.sprite = arcade.Sprite(
+            ":resources:images/enemies/mouse.png",
+            tile_size / sprite_orig_size,
+            center_x=x,
+            center_y=y + self.offset_y
         )
 
-    def respawn(self, snake_placement=None):
+    def draw(self):
+        self.sprite.draw()
+
+    def new_position(self, snake_placement):
         while True:
-            X = randrange(
-                SQUARE_SIZE / 2, SCREEN_WIDTH - (SQUARE_SIZE / 2), SQUARE_SIZE
+            width, height = self.window_dimensions
+
+            x = randrange(
+                self.tile_size / 2,
+                width - (self.tile_size / 2),
+                self.tile_size
             )
-            Y = randrange(
-                SQUARE_SIZE / 2, SCREEN_HEIGHT - (SQUARE_SIZE / 2), SQUARE_SIZE
+            y = randrange(
+                self.tile_size / 2,
+                height - (self.tile_size / 2),
+                self.tile_size
             )
 
             overlaps = False
 
-            if (snake_placement):
-                for x, y in snake_placement:
-                    overlaps = x == X and y == Y
+            for s_x, s_y in snake_placement:
+                if s_x == x and s_y == y:
+                    overlaps = True
+                    break
 
-            if overlaps:
-                continue
-            else:
-                break
+            if not overlaps:
+                return x, y
 
-        self.x = X
-        self.y = Y
+    def respawn(self, snake_placement):
+        x, y = self.new_position(snake_placement)
 
-    def get_position(self):
-        return self.x, self.y
+        self.sprite.center_x = x
+        self.sprite.center_y = y + self.offset_y  # keep offset in mind
+
+    @property
+    def position(self):
+        # subtract offset since we only need it when displaying
+        return self.sprite.center_x, self.sprite.center_y - self.offset_y
 
 
 class Snake():
-    def __init__(self, size):
-        self.size = size
-        self.speed = size
+    def __init__(self, window_dimensions, tile_size):
+        self.tile_size = tile_size
+        self.window_dimensions = window_dimensions
+
+        self.size = tile_size
+        self.speed = tile_size
 
         self._direction = None
         self.direction_locked = False
 
+        width, height = self.window_dimensions
+
         # list of tuples (x, y)
         self.body = [
-            ((SCREEN_WIDTH / 2) - (SQUARE_SIZE / 2),
-             (SCREEN_HEIGHT / 2) - (SQUARE_SIZE / 2))
+            ((width / 2) - (tile_size / 2),
+             (height / 2) - (tile_size / 2))
         ]
 
     def reset(self):
         self._direction = None
         self.direction_locked = False
 
+        width, height = self.window_dimensions
+
         # list of tuples (x, y)
         self.body = [
-            ((SCREEN_WIDTH / 2) - (SQUARE_SIZE / 2),
-             (SCREEN_HEIGHT / 2) - (SQUARE_SIZE / 2))
+            ((width / 2) - (self.tile_size / 2),
+             (height / 2) - (self.tile_size / 2))
         ]
 
     @property
@@ -99,13 +131,13 @@ class Snake():
 
     @property
     def opposite_direction(self):
-        if self._direction == UP:
+        if self.direction == UP:
             return DOWN
-        elif self._direction == DOWN:
+        elif self.direction == DOWN:
             return UP
-        elif self._direction == LEFT:
+        elif self.direction == LEFT:
             return RIGHT
-        elif self._direction == RIGHT:
+        elif self.direction == RIGHT:
             return LEFT
         else:
             return None
@@ -123,11 +155,11 @@ class Snake():
     def move(self):
         x, y = self.body[-1]  # head position
 
-        if self._direction == UP:
+        if self.direction == UP:
             y += self.speed
-        elif self._direction == DOWN:
+        elif self.direction == DOWN:
             y -= self.speed
-        elif self._direction == LEFT:
+        elif self.direction == LEFT:
             x -= self.speed
         else:
             x += self.speed
@@ -138,9 +170,9 @@ class Snake():
 
         self.direction_locked = False
 
-    def did_eat(self, apple):
+    def did_eat(self, apple_position):
         X, Y = self.body[-1]  # head position
-        x, y = apple.get_position()
+        x, y = apple_position
 
         if X == x and Y == y:
             self.body.append((x, y))
@@ -148,14 +180,14 @@ class Snake():
 
         return False
 
-    def next_position(self):
+    def next_move(self):
         x, y = self.body[-1]  # head position
 
-        if self._direction == UP:
+        if self.direction == UP:
             y += self.speed
-        elif self._direction == DOWN:
+        elif self.direction == DOWN:
             y -= self.speed
-        elif self._direction == LEFT:
+        elif self.direction == LEFT:
             x -= self.speed
         else:
             x += self.speed
@@ -163,7 +195,7 @@ class Snake():
         return x, y
 
     def collides_self(self):
-        X, Y = self.next_position()
+        X, Y = self.next_move()
 
         for x, y in self.body:
             if x == X and y == Y:
@@ -173,21 +205,23 @@ class Snake():
 
 
 class Pynake(arcade.Window):
-    def __init__(self, width, height):
+    def __init__(self, width, height, tile_size):
         super().__init__(width, height, 'Snake')
+
+        self.tile_size = tile_size
 
         self.game_started = False
         self.game_over = False
 
-        self.snake = Snake(SQUARE_SIZE)
-        self.apple = Apple(SQUARE_SIZE)
+        self.snake = Snake((width, height), tile_size)
+        self.apple = Apple((width, height), tile_size, self.snake.placement)
 
         # goes clockwise (top, right, bottom, left)
         self.borders = (
-            int(SCREEN_HEIGHT - SQUARE_SIZE / 2),
-            int(SCREEN_WIDTH - SQUARE_SIZE / 2),
-            int(0 + SQUARE_SIZE / 2),
-            int(0 + SQUARE_SIZE / 2),
+            int(height - tile_size / 2),
+            int(width - tile_size / 2),
+            int(0 + tile_size / 2),
+            int(0 + tile_size / 2),
         )
 
         # If you have sprite lists, you should create them here,
@@ -231,7 +265,7 @@ class Pynake(arcade.Window):
 
         self.snake.move()
 
-        if self.snake.did_eat(self.apple):
+        if self.snake.did_eat(self.apple.position):
             self.apple.respawn(self.snake.placement)
 
     def on_key_press(self, key, key_modifiers):
@@ -251,7 +285,7 @@ class Pynake(arcade.Window):
             self.restart()
 
     def hits_border(self):
-        x, y = self.snake.next_position()
+        x, y = self.snake.next_move()
         top, right, bottom, left = self.borders
 
         if x < left or x > right or y < bottom or y > top:
@@ -263,14 +297,14 @@ class Pynake(arcade.Window):
         self.game_over = False
         self.game_started = False
         self.snake.reset()
-        self.apple.respawn()
+        self.apple.respawn(self.snake.placement)
 
     def draw_background(self):
         color = arcade.color.ANDROID_GREEN
 
-        for y in range(int(SQUARE_SIZE / 2), int(SCREEN_HEIGHT - SQUARE_SIZE / 2) + 1, SQUARE_SIZE):
-            for x in range(int(SQUARE_SIZE / 2), int(SCREEN_WIDTH - SQUARE_SIZE / 2) + 1, SQUARE_SIZE):
-                arcade.draw_point(x, y, color, SQUARE_SIZE)
+        for y in range(int(self.tile_size / 2), int(self.height - self.tile_size / 2) + 1, self.tile_size):
+            for x in range(int(self.tile_size / 2), int(self.width - self.tile_size / 2) + 1, self.tile_size):
+                arcade.draw_point(x, y, color, self.tile_size)
                 color = (arcade.color.ANDROID_GREEN if color != arcade.color.ANDROID_GREEN
                          else arcade.color.APPLE_GREEN)
 
@@ -280,8 +314,8 @@ class Pynake(arcade.Window):
     def draw_game_over(self):
         arcade.draw_text(
             "GAME OVER",
-            SCREEN_WIDTH / 2,
-            SCREEN_HEIGHT / 2,
+            self.width / 2,
+            self.height / 2,
             arcade.color.RED,
             40,
             align="center",
@@ -291,7 +325,7 @@ class Pynake(arcade.Window):
 
 
 def main():
-    game = Pynake(SCREEN_WIDTH, SCREEN_HEIGHT)
+    game = Pynake(SCREEN_WIDTH, SCREEN_HEIGHT, TILE_SIZE)
     game.setup().run()
 
 
